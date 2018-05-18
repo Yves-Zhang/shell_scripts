bd5f0c8fc66496cf157029983080987693a9c292
Yves-Zhang <zhangyi51@baidu.com>
2018-05-18 11:43:37 +0800

    add abs_path.sh

---
+++ abs_path.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+abs_path() {
+    SOURCE="${BASH_SOURCE[0]}"
+    while [ -h "$SOURCE" ]; do
+        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+    done
+    echo "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+}
+
+BIN=`abs_path`
